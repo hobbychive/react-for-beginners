15e10ce8e61d652bc80ad4527b7bdd1d99f02ab9
hobbychive <parksw123098@gmail.com>
2023-02-06 15:44:08 +0900

    updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./routes/Home";
 import Detail from "./routes/Detail";
-
+import Home from "./routes/Home";
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route
-          path={`${process.env.PUBLIC_URL}/movie/:id`}
-          element={<Detail />}
-        ></Route>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
+        <Route path="/abot-us" element={<h1>Hello</h1>}></Route>
+        <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
   );
